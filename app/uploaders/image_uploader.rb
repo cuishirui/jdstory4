36142376093c4dae3a5272ccd932a92120ab5633
huwenjing <1035344285@qq.com>
2017-03-17 21:49:36 +0800

    change admin/product backend panel

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -13,12 +13,13 @@ include CarrierWave::MiniMagick
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
   process resize_to_fit: [800, 800]
+  
   version :thumb do
     process resize_to_fill: [200,200]
   end
 
   version :medium do
-    peocess resize_to_fill: [400,400]
+    process resize_to_fill: [400,400]
   end
   # Provide a default URL as a default if there hasn't been a file uploaded:
   # def default_url
